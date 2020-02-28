68b1c228200063d435a3a5d374b1b2ba18a91e6a
Vali <bk18529@bristol.ac.uk>
2020-02-28 22:42:45 +0200

    First commit

---
+++ src/main/java/com/example/Skylink/SkylinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Skylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkylinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkylinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Skylink/SkylinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Skylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
